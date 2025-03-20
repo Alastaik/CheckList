dd76e3d74a456ec6798fd579e94a4c46a936e318
Alastaik <138844507+Alastaik@users.noreply.github.com>
2025-03-20 10:10:14 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document is ready!');
 
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+
+    if (menuIcon && menu) {
+        menuIcon.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+        });
+    }
+});
