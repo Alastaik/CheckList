d4446e3f6cf9922e9074660b7ea3d5ea2c22d2d5
Alastaik <138844507+Alastaik@users.noreply.github.com>
2025-03-20 11:27:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// filepath: c:\Users\User\Documents\my-website\server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
